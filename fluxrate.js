1b018c020e2543879481fb2ee5cc402a7ee72b7e
dartfalak <falakcles@gmail.com>
2025-10-07 22:58:46 +0500

    add currency conversion logic

--- fluxrate.js
+++ fluxrate.js
@@ -1,7 +1,32 @@
 
-  const url = "https://api.exchangerate-api.com/v4/latest/USD";
+const url = "https://api.exchangerate-api.com/v4/latest/USD";
+const currencyselection = document.querySelectorAll(".currency-selection")
+const amountInput = document.getElementById('amount');
+const fromCurrency = document.getElementById('from');
+const toCurrency = document.getElementById('to');
+const fetchBtn = document.getElementById('fetch');
+const msg = document.querySelector('.msg'); 
 
-  const currencyselection = document.querySelectorAll(".currency-selection")
+
+const rates = {
+  USD: 1,
+  PKR: 281,
+  EUR: 0.92
+};
 
 
-   
+fetchBtn.addEventListener('click', () => {
+  const amount = parseFloat(amountInput.value);
+  const from = fromCurrency.value;
+  const to = toCurrency.value;
+
+  if (!amount || amount <= 0) {
+    msg.textContent = '⚠️ Please enter a valid amount!';
+    return;
+  }
+
+  
+  const converted = (amount / rates[from]) * rates[to];
+
+  msg.textContent = `${amount} ${from} = ${converted.toFixed(2)} ${to}`;
+});
